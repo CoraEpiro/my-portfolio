25a0d4952f8c1722751da33939826b4a2e78e778
CoraEpiro <aguliyev45@gmail.com>
2025-06-22 02:13:22 +0200

    Revert sitemap to correct www.aliguliyev.com URLs (primary domain)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://aliguliyev.com'
+  const baseUrl = 'https://www.aliguliyev.com'
   
   return {
     rules: {
